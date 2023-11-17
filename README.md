8b4ab113f069f759c74adee4e7a11a89aadbeab4
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-11-18 06:55:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**HOW TO INSTALL**
+
+Run this in ur CLI:
+
+pip install flask
+pip install flask-login
+pip install flask-sqlalchemy
---
+++ app/auth/views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from . import auth
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
